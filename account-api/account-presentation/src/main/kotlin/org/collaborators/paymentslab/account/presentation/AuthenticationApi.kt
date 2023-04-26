62a085e4c56f7cb2c3f23944cf5bd45e031600d8
wanniDev <wannidev0928@gmail.com>
2023-04-26 11:32:33 +0900

    Feat: 회원가입 api 작성

---
+++ account-api/account-presentation/src/main/kotlin/org/collaborators/paymentslab/account/presentation/AuthenticationApi.kt
@@ -0,0 +1,19 @@
+package org.collaborators.paymentslab.account.presentation
+
+import org.collaborators.paymentslab.account.application.AccountService
+import org.collaborators.paymentslab.account.application.command.RegisterAccount
+import org.collaborators.paymentslab.account.presentation.request.RegisterAccountRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/auth")
+class AuthenticationApi(private val accountService: AccountService) {
+
+    @PostMapping("register")
+    fun register(@RequestBody request: RegisterAccountRequest) {
+        accountService.register(RegisterAccount(request.email, request.password, request.username))
+    }
+}
