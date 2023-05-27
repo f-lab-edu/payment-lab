ba9cfeb842cbcc97823e387bc45f30841db72358
wanniDev <wannidev0928@gmail.com>
2023-05-27 23:14:41 +0900

    Fix: RestControllerAdvice에서 관리하는 에러메세지를 동작안되는 버그를 수정하기 위해, JWT 예외 catch 표현식에 jwt 계열 예외로 수정
    기존에는 JWT 필터 예외 테스트를 해본다고 Exception을 활용했는데,
    그로인해 jwt 토큰으로 인증을 우선 시행하는 api를 활용했을 때 예외가 발생하면 필터에서 던지는 예외로 코드가 끊긴다.
    
    그래서, 어떤 예외가 발생하든 invalid jwt exception이 발생하는 현상이 발생해서 디버깅이 어려웠다.

--- app/src/main/kotlin/org/collaborators/paymentslab/config/security/JwtExceptionFilter.kt
+++ app/src/main/kotlin/org/collaborators/paymentslab/config/security/JwtExceptionFilter.kt
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.collaborator.paymentlab.common.error.ErrorCode
+import org.collaborator.paymentlab.common.error.InvalidTokenException
 import org.collaborator.paymentlab.common.result.ApiResult
 import org.collaborators.paymentslab.account.infrastructure.jwt.exception.AlreadyTokenExpiredException
 import org.springframework.http.HttpStatus
@@ -19,11 +20,20 @@ class JwtExceptionFilter(private val objectMapper: ObjectMapper): OncePerRequest
     ) {
         try {
             filterChain.doFilter(request, response)
-        } catch (exception: Exception) {
-            response.status = HttpStatus.BAD_REQUEST.value()
-            response.contentType = "application/json; charset=UTF-8"
-            response.writer.write(objectMapper.writeValueAsString(ApiResult.error(extractErrorCode(exception))))
+        } catch (exception: InvalidTokenException) {
+            streamErrorResponse(response, exception)
+        } catch (exception: AlreadyTokenExpiredException) {
+            streamErrorResponse(response, exception)
         }
+    }
+
+    private fun streamErrorResponse(
+        response: HttpServletResponse,
+        exception: Exception
+    ) {
+        response.status = HttpStatus.BAD_REQUEST.value()
+        response.contentType = "application/json; charset=UTF-8"
+        response.writer.write(objectMapper.writeValueAsString(ApiResult.error(extractErrorCode(exception))))
     }
 
     private fun extractErrorCode(exception: Exception): ErrorCode {
