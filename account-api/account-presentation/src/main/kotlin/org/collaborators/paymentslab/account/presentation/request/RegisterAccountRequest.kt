598ce90671dcc921b3a0d60b5dbc64035264926a
wanniDev <wannidev0928@gmail.com>
2023-07-08 22:00:05 +0900

    Feat: 휴대폰 번호 입력 포맷 검증 정규식 추가

--- account-api/account-presentation/src/main/kotlin/org/collaborators/paymentslab/account/presentation/request/RegisterAccountRequest.kt
+++ account-api/account-presentation/src/main/kotlin/org/collaborators/paymentslab/account/presentation/request/RegisterAccountRequest.kt
@@ -2,6 +2,7 @@ package org.collaborators.paymentslab.account.presentation.request
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 import org.collaborators.paymentslab.account.presentation.validation.Password
 
 data class RegisterAccountRequest(
@@ -11,6 +12,6 @@ data class RegisterAccountRequest(
     val password: String,
     @field:NotBlank
     val username: String,
-    @field:NotBlank
+    @field:NotBlank @field: Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$")
     val phoneNumber: String
 )
