697a62dcf5ba4f593d44d6c7e683f4e2dea6ce3b
wanniDev <wannidev0928@gmail.com>
2023-08-05 13:20:20 +0900

    Feat: 결제 결과 및 이력 로그 파일저장 기능 구현

--- payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/tosspayments/DefaultPaymentsProcessor.kt
+++ payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/tosspayments/DefaultPaymentsProcessor.kt
@@ -1,23 +1,30 @@
 package org.collaborators.paymentslab.payment.infrastructure.tosspayments
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.collaborator.paymentlab.common.AuthenticatedUser
 import org.collaborators.paymentslab.payment.domain.PaymentHistory
 import org.collaborators.paymentslab.payment.domain.PaymentHistoryRepository
 import org.collaborators.paymentslab.payment.domain.PaymentsProcessor
 import org.collaborators.paymentslab.payment.domain.TossPaymentsRepository
+import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.transaction.annotation.Transactional
 
-class DefaultPaymentsProcessor(
+open class DefaultPaymentsProcessor(
     private val tossPaymentsKeyInApprovalProcessor: TossPaymentsKeyInApprovalProcessor,
     private val tossPaymentsRepository: TossPaymentsRepository,
 
     // TODO 결제이력 eventListener 구현 이후 삭제
     private val paymentHistoryRepository: PaymentHistoryRepository,
 
-    private val publisher: ApplicationEventPublisher
+    private val publisher: ApplicationEventPublisher,
+    private val objectMapper: ObjectMapper
 ): PaymentsProcessor {
 
+    private val logger = LoggerFactory.getLogger("org.collaborators.paymentslab")
+
+    @Transactional
     override fun keyInPay(
         amount: Int,
         orderId: String,
@@ -47,8 +54,11 @@ class DefaultPaymentsProcessor(
         newPaymentEntity.completeOf(principal.id)
         val newPaymentRecord = tossPaymentsRepository.save(newPaymentEntity)
 
+        logger.info("complete {}", objectMapper.writeValueAsString(newPaymentRecord))
         val newPaymentHistoryEntity = PaymentHistory.newInstanceFrom(newPaymentRecord)
         paymentHistoryRepository.save(newPaymentHistoryEntity)
+        logger.info("history {}",objectMapper.writeValueAsString(newPaymentHistoryEntity))
+
         // TODO EventListener 를 활용하여, 비동기로 결제이력 저장하는 코드 작성
 //        newPaymentRecord.pollAllEvents().forEach { publisher.publishEvent(it) }
     }
