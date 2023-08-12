e3f4b17694640f33a5638e49e4776df6eaddfe81
wanniDev <wannidev0928@gmail.com>
2023-08-12 21:36:31 +0900

    Feat: 주문 결제 승인 데이터접근 계층 구현체 구성

--- payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/PaymentModuleConfiguration.kt
+++ payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/PaymentModuleConfiguration.kt
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import org.collaborators.paymentslab.payment.infrastructure.jpa.PaymentOrderRepositoryAdapter
 import org.collaborators.paymentslab.payment.infrastructure.jpa.TossPaymentHistoryRepositoryAdapter
 import org.collaborators.paymentslab.payment.infrastructure.jpa.TossPaymentsRepositoryAdapter
 import org.collaborators.paymentslab.payment.infrastructure.tosspayments.DefaultPaymentsProcessor
@@ -24,6 +25,7 @@ import java.time.format.DateTimeFormatter
 @Import(value = [
     TossPaymentsRepositoryAdapter::class,
     TossPaymentHistoryRepositoryAdapter::class,
+    PaymentOrderRepositoryAdapter::class,
     DefaultPaymentsProcessor::class,
     TossPaymentsQueryManager::class,
     TossPaymentsKeyInApprovalProcessor::class,
---
+++ payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/jpa/PaymentOrderRepositoryAdapter.kt
@@ -0,0 +1,17 @@
+package org.collaborators.paymentslab.payment.infrastructure.jpa
+
+import org.collaborators.paymentslab.payment.domain.entity.PaymentOrder
+import org.collaborators.paymentslab.payment.domain.repository.PaymentOrderRepository
+
+
+class PaymentOrderRepositoryAdapter(
+    private val jpaPaymentOrderRepository: JpaPaymentOrderRepository
+): PaymentOrderRepository {
+    override fun save(entity: PaymentOrder): PaymentOrder {
+        return jpaPaymentOrderRepository.save(entity)
+    }
+
+    override fun findById(id: Long): PaymentOrder {
+        return jpaPaymentOrderRepository.findPaymentOrderById(id)
+    }
+}
