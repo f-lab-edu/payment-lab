194eb262514905747c17cc37b3ed7bdd71da3fdf
wanniDev <wannidev0928@gmail.com>
2023-08-13 12:19:55 +0900

    Test: 추가된 기능을 테스트를 위해 기존 키인 결제 api에 주문 결제 검증 추가

--- app/src/test/kotlin/org/collaborators/paymentslab/payment/presentation/mock/MockPaymentService.kt
+++ app/src/test/kotlin/org/collaborators/paymentslab/payment/presentation/mock/MockPaymentService.kt
@@ -6,19 +6,28 @@ import org.collaborators.paymentslab.payment.application.query.PaymentHistoryQue
 import org.collaborators.paymentslab.payment.application.query.PaymentHistoryQueryQueryModel
 import org.collaborators.paymentslab.payment.domain.PaymentsProcessor
 import org.collaborators.paymentslab.payment.domain.PaymentsQueryManager
+import org.collaborators.paymentslab.payment.domain.repository.PaymentOrderRepository
+import org.collaborators.paymentslab.payment.infrastructure.tosspayments.TossPaymentsValidator
 import org.collaborators.paymentslab.payment.infrastructure.tosspayments.exception.TossPaymentsApiClientException
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.nio.charset.StandardCharsets
+import java.util.*
 
 @Service
 @Profile(value = ["test"])
 @Transactional
 class MockPaymentService(
     private val paymentsProcessor: PaymentsProcessor,
-    private val paymentsQueryManager: PaymentsQueryManager
+    private val paymentsQueryManager: PaymentsQueryManager,
+    private val paymentOrderRepository: PaymentOrderRepository,
+    private val tossPaymentsValidator: TossPaymentsValidator
 ): PaymentService(paymentsProcessor, paymentsQueryManager) {
-    override fun keyInPay(command: TossPaymentsKeyInPayCommand) {
+    override fun keyInPay(paymentOrderId: Long, command: TossPaymentsKeyInPayCommand) {
+        val paymentOrder = paymentOrderRepository.findById(paymentOrderId)
+        tossPaymentsValidator.validate(paymentOrder, command.amount, command.orderName)
+
         val cardNumber = command.cardNumber
         if (cardNumber.length != 16 || !isDigits(cardNumber))
             throw TossPaymentsApiClientException(MockTossPaymentsErrorBody.invalidCardNumber)
