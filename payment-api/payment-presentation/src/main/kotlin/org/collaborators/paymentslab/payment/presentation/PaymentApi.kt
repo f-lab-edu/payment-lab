e5cb347de4c86a52066fac4788a0f3d91ccd8860
wanniDev <wannidev0928@gmail.com>
2023-09-16 11:21:42 +0900

    Feat: paymentOrder 발급 api 시그니처 구현
    1. 사용자가 물건을 고른다.
    2. 장바구니 페이지를 통해 '구매하기'를 클릭하거나 상세 페이지에서 '바로 구매하기'를 클릭한다. (사용자의 구매 확정시점)
    3. 해당 상품의 구매정보(현 시점에선 구매 상품 주문 명, 구매 수량)를 바탕으로 발급번호 발행
    4. 발행한 발급번호를 토대로 주문 결제를 수행하는 uri로 리다이렉트

--- payment-api/payment-presentation/src/main/kotlin/org/collaborators/paymentslab/payment/presentation/PaymentApi.kt
+++ payment-api/payment-presentation/src/main/kotlin/org/collaborators/paymentslab/payment/presentation/PaymentApi.kt
@@ -6,8 +6,12 @@ import org.collaborators.paymentslab.payment.application.PaymentService
 import org.collaborators.paymentslab.payment.application.query.PaymentHistoryQuery
 import org.collaborators.paymentslab.payment.presentation.request.TossPaymentsKeyInRequest
 import org.collaborators.paymentslab.payment.presentation.response.PaymentHistoryResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping("api/v1/toss-payments")
@@ -20,6 +24,15 @@ class PaymentApi(private val paymentService: PaymentService) {
         @RequestBody @Valid request: TossPaymentsKeyInRequest
     ) {
         paymentService.keyInPay(paymentOrderId, request.toCommand())
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @PostMapping("payment-order")
+    fun generatePaymentOrder(
+        @RequestBody @Valid request: PaymentOrderRequest
+    ): ResponseEntity<Void> {
+        val paymentOrderId = paymentService.generatePaymentOrder()
+        return ResponseEntity.status(HttpStatus.SEE_OTHER).location(URI("/api/v1/toss-payments/${paymentOrderId}")).build()
     }
 
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
