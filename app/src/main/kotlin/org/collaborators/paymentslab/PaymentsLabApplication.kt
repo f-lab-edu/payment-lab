02d3ecbe9c069fe861a8cfda5be68cc8dbc2e7a9
wanniDev <wannidev0928@gmail.com>
2023-11-11 21:36:46 +0900

    Feat: prod 환경에서는 로그 출력레벨을 debug로 지정하지 못하게 하는 코드 추가

--- app/src/main/kotlin/org/collaborators/paymentslab/PaymentsLabApplication.kt
+++ app/src/main/kotlin/org/collaborators/paymentslab/PaymentsLabApplication.kt
@@ -1,11 +1,38 @@
 package org.collaborators.paymentslab
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.core.env.ConfigurableEnvironment
 
 @SpringBootApplication
-class PaymentsLabApplication
+class PaymentsLabApplication: ApplicationListener<ApplicationReadyEvent> {
+	override fun onApplicationEvent(event: ApplicationReadyEvent) {
+		val env = event.applicationContext.environment
+		val rootLoggingLevel = env.getProperty("logging.level.root", String::class.java, "NOT_SET")
+		val applicationLoggingLevel = env
+			.getProperty("logging.level.org.collaborators.paymentslab.*", String::class.java, "NOT_SET")
+
+		if (isProfileProduction(env) && isLoggingLevelDebug(rootLoggingLevel, applicationLoggingLevel)) {
+			throw IllegalStateException("production 환경에서는 로그 출력 레벨을 'debug'로 설정해선 안됩니다.")
+		}
+	}
+
+	private fun isLoggingLevelDebug(loggingLevel: String, applicationLoggingLevel: String) =
+		loggingLevel.equals("DEBUG", ignoreCase = true)
+				|| applicationLoggingLevel.equals("DEBUG", ignoreCase = true)
+
+	private fun isProfileProduction(env: ConfigurableEnvironment) = "prod" in env.activeProfiles
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(PaymentsLabApplication::class.java, *args)
+		}
+	}
+}
 
 fun main(args: Array<String>) {
-	runApplication<PaymentsLabApplication>(*args)
+	PaymentsLabApplication.main(args)
 }
