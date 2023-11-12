d2117b3071a30c29122f78644b894cddf5af182d
wanniDev <wannidev0928@gmail.com>
2023-11-12 15:35:46 +0900

    Feat: 정해진 프로파일만 사용하여 프로파일 지정에 대한 일관성 유지

--- app/src/main/kotlin/org/collaborators/paymentslab/PaymentsLabApplication.kt
+++ app/src/main/kotlin/org/collaborators/paymentslab/PaymentsLabApplication.kt
@@ -14,16 +14,26 @@ class PaymentsLabApplication: ApplicationListener<ApplicationReadyEvent> {
 		val applicationLoggingLevel = env
 			.getProperty("logging.level.org.collaborators.paymentslab.*", String::class.java, "NOT_SET")
 
-		if (isProfileProduction(env) && isLoggingLevelDebug(rootLoggingLevel, applicationLoggingLevel)) {
+		if (!isProfileInBoundary(env))
+			throw IllegalStateException("profile은 'local', 'test', 'dev', 'prod' 중 하나로 지정 해야합니다.")
+
+		if (isProfileProduction(env) && isLoggingLevelDebug(rootLoggingLevel, applicationLoggingLevel))
 			throw IllegalStateException("production 환경에서는 로그 출력 레벨을 'debug'로 설정해선 안됩니다.")
-		}
 	}
 
 	private fun isLoggingLevelDebug(loggingLevel: String, applicationLoggingLevel: String) =
 		loggingLevel.equals("DEBUG", ignoreCase = true)
 				|| applicationLoggingLevel.equals("DEBUG", ignoreCase = true)
 
+	private fun isProfileInBoundary(env: ConfigurableEnvironment)
+		= isProfileLocal(env) || isProfileTest(env) || isProfileDev(env) || isProfileProduction(env)
+
 	private fun isProfileProduction(env: ConfigurableEnvironment) = "prod" in env.activeProfiles
+
+	private fun isProfileDev(env: ConfigurableEnvironment) = "dev" in env.activeProfiles
+
+	private fun isProfileTest(env: ConfigurableEnvironment) = "test" in env.activeProfiles
+	private fun isProfileLocal(env: ConfigurableEnvironment) = "local" in env.activeProfiles
 
 	companion object {
 		@JvmStatic
