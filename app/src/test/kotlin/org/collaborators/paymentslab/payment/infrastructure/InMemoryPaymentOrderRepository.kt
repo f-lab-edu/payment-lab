9439d6c33a7818ef941d45ecb8b465711e74a5a1
wanniDev <wannidev0928@gmail.com>
2023-12-06 12:13:27 +0900

    Remove: Mockito로도 충분히 모킹이 가능하므로 실수로 인한 생산성 저하를 야기시킬 수 있는 in-memory-repository 삭제

--- app/src/test/kotlin/org/collaborators/paymentslab/payment/infrastructure/InMemoryPaymentOrderRepository.kt
+++
@@ -1,9 +0,0 @@
-package org.collaborators.paymentslab.payment.infrastructure
-
-import org.collaborators.paymentslab.payment.domain.entity.PaymentOrder
-import org.collaborator.paymentlab.common.inmemory.core.InMemoryRepository
-import org.springframework.stereotype.Component
-
-@Component
-class InMemoryPaymentOrderRepository: InMemoryRepository<PaymentOrder, Long>(Long::class.java) {
-}
