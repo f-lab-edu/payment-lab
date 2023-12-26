ac21b18ce86cf035889dd04dd6d96562473a5861
wanniDev <wannidev0928@gmail.com>
2023-12-26 16:20:47 +0900

    Feat: payment toggle이 비활성화 될 경우, 테스트용 결제승인을 수행하게하는 함수 구현

--- payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/tosspayments/TossPaymentsRestClient.kt
+++ payment-api/payment-infrastructure/src/main/kotlin/org/collaborators/paymentslab/payment/infrastructure/tosspayments/TossPaymentsRestClient.kt
@@ -1,6 +1,9 @@
 package org.collaborators.paymentslab.payment.infrastructure.tosspayments
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.collaborator.paymentlab.common.domain.RestClient
+import org.collaborators.paymentslab.payment.domain.entity.PaymentsStatus
+import org.collaborators.paymentslab.payment.infrastructure.togglz.PaymentFeature
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpEntity
 import org.springframework.http.ResponseEntity
@@ -16,6 +19,41 @@ class TossPaymentsRestClient(
         url: String,
         request: HttpEntity<TossPaymentsKeyInDto>
     ): ResponseEntity<TossPaymentsApprovalResponse> {
-        return restTemplate.postForEntity(url, request, TossPaymentsApprovalResponse::class.java)
+        if (PaymentFeature.TOSS_PAYMENTS_FEATURE.isActive())
+            return restTemplate.postForEntity(url, request, TossPaymentsApprovalResponse::class.java)
+
+        // 테스트용 결제승인 요청을 위해 RestClient 를 컴포지션 패턴으로 재정의
+        val client = object : RestClient<TossPaymentsKeyInDto, TossPaymentsApprovalResponse> {
+            override fun keyIn(url: String, request: HttpEntity<TossPaymentsKeyInDto>): ResponseEntity<TossPaymentsApprovalResponse> {
+                val reqBody = request.body ?: throw NullPointerException()
+                val objectMapper = ObjectMapper()
+                Thread.sleep((300L..1000L).random())
+                return ResponseEntity.ok(TossPaymentsApprovalResponse(
+                    mId = "tvivarepublica4",
+                    lastTransactionKey = "2A441542485089863EB31F9B039FEFF8",
+                    paymentKey = objectMapper.writeValueAsString(reqBody),
+                    orderId = reqBody.orderId,
+                    orderName = reqBody.orderName,
+                    taxExemptionAmount = 0,
+                    status = PaymentsStatus.DONE.name,
+                    useEscrow = false,
+                    cultureExpense = false,
+                    card = TossPaymentsCardInfoResponse(
+                        issuerCode = "4V",
+                        acquirerCode = "21",
+                        number = reqBody.cardNumber,
+                        installmentPlanMonths = 0,
+                        isInterestFree = false,
+                        approveNo = "00000000",
+                        useCardPoint = false,
+                        cardType = "미확인",
+                        ownerType =  "미확인",
+                        acquireStatus = "READY",
+                        amount = reqBody.amount
+                    )
+                ))
+            }
+        }
+        return client.keyIn(url, request)
     }
 }
